d1b15a30e569b6d43e5b1351160370890e5d324e
Simon Zhen <me@PeeSee.local>
2017-04-04 02:34:57 -0700

    reduced scroll view width

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        //width of the scroll view
+        var scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")  //icon1,icon2,icon3; raw image
@@ -22,15 +29,19 @@ class ViewController: UIViewController {
             images.append(imageView)
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
+            contentWidth += newX
+            
+            //make scrollable
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
-        print("Count: \(images.count)")
+        //don't hide objects outside of scroll view
+        scrollView.clipsToBounds = false
+        
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
-
-
 }
 
