07f92519cd0c47c097af0ba6b0065adb61cd6c23
Simon Zhen <me@PeeSee.local>
2017-04-04 03:03:16 -0700

    minor fixes

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -18,30 +18,23 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        var contentWidth: CGFloat = 0.0
         
-        //width of the scroll view
-        var scrollWidth = scrollView.frame.size.width
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")  //icon1,icon2,icon3; raw image
             let imageView = UIImageView(image: image)   //image applied to an imageview object
             images.append(imageView)
             
-            var newX: CGFloat = 0.0
-            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
-            
-            contentWidth += newX
+            let newX: CGFloat = scrollWidth / 2 + scrollWidth * CGFloat(x) - 75
             
             //make scrollable
+            imageView.frame = CGRect(x: newX, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         //don't hide objects outside of scroll view
         scrollView.clipsToBounds = false
-        
-        
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.contentSize = CGSize(width: scrollView.frame.width * CGFloat(3), height: view.frame.size.height)
     }
 }
 
